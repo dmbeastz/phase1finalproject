c448a72349c4ff5c573d0be63239c2e734628c92
Dave <daveroymutisya2@gmail.com>
2023-10-25 19:53:25 +0300

    Started Javascript defining variables

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
+//Testing to seeing if the api server 
+
+fetch("https://api.unsplash.com/search/photos") 
+.then(res => {
+    if (res.ok) {
+        console.log ("successful")
+    }
+   
+else {console.log ("Not successful")}
+res.json()})
+.then (data =>console.log (data))
+.catch (error =>console.log ('ERROR'));
+
+//Access Key
+const accessskey = 'HuLYymNuMZ_DGABtlB5eg94a19uP1n6riYOmJXMp1Ew';
+
+const formEl = document.querySelector ("form");
+const searchinput = document.getElementById ("search-input");
 
 
