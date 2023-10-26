0f4b3c9eb60a779f483b2e212314ab0787f3e5d0
Dave <daveroymutisya2@gmail.com>
2023-10-26 16:28:34 +0300

    Completed project

--- js/script.js
+++ js/script.js
@@ -2,43 +2,68 @@
 
 // Creating input data that can be stored
 let inputData = ""
-
+let imagesearch = document.getElementById('imageresults')
 // Access Key
 const accesskey = "HuLYymNuMZ_DGABtlB5eg94a19uP1n6riYOmJXMp1Ew";
 // //DOM
  const formEl = document.querySelector ("form");
 const searchinput = document.getElementById ("search-input");
-const searchResults = document.querySelector(".search-result");
+const searchResults = document.querySelector(".search-results");
 const viewmore = document.getElementById ("view-more-button");
 
 let page = 1;
-
+// imagesearch.style='display:none'
  formEl.addEventListener('submit', (event) =>
  {
      event.preventDefault()
      searchImages(searchinput.value, page)
+    // imagesearch.style.display = 'initial';
  })
 
  function searchImages(inputValue, page)
 {
-  console.log(inputValue)
- console.log(page)
-console.log(accesskey)
 
  fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${inputValue}&client_id=${accesskey}`)
  .then(response => response.json())
 .then(pictureData => 
  {
-    console.log(pictureData['results'][0]['urls']['regular'])
+    let {results}=pictureData
 
-    let imagesearch = document.getElementById('imageresults')
-    imagesearch.src =pictureData['results'][0]['urls']['regular']
-    //searchResults.appendChild(imagesearch)
+    results.forEach(result => 
+    {
+        let {alt_description,urls:{regular}}=result
+
+        let searchResult=document.createElement("div")
+        searchResult.setAttribute("class","search-result")
+
+        searchResult.innerHTML=
+        `
+        <img id ="imageresults" src="${regular}" alt="${alt_description}">
+        <p>${alt_description}</p> 
+        `
+        console.log(searchResult)
+        searchResults.appendChild(searchResult)
+
+    });
+
+  
+
+    // pictureData.forEach(pictureData => 
+    // {
+    //     let [{description,urls :{raw} }]=pictureData
+    //     console.log(description)
+    //     console.log(raw)
+    // });
+    // console.log(pictureData['results'][0]['urls']['regular'])
+ 
+    
+    // imagesearch.src =pictureData['results'][0]['urls']['regular']
+    
     
 
-    result.innerHTML = {
+    // results.innerHTML = {
         
-    }
+    // }
     
 
             
@@ -46,32 +71,32 @@ console.log(accesskey)
                 
   })
 
-    if (page === 1){
-        viewmore.style.display = "block"
-    }
+    // if (page === 1){
+    //     viewmore.style.display = "block"
+    // }
 
 //Creating and Pushing all the data into the template created in the HTML ("div")
-results.map ((result)=> {
-    const imageWrapper = document.createElement('div');
-    imageWrapper.classList.add (".search-result");
-    const image = document.getElementById ("img");
-    image.src = results.urls.small;//Get the image
-    image.alt = results.alt_description;//Get the description
-    const imageLink = document.createElement("a");
-    imageLink.href = results.links.html;//Get the link for a spefic image
-    imageLink.target = "_blank";
-    image.textContent = result.alt_description;
+// results.map ((result)=> {
+//     const imageWrapper = document.createElement('div');
+//     imageWrapper.classList.add (".search-result");
+//     const image = document.getElementById ("img");
+//     image.src = results.urls.small;//Get the image
+//     image.alt = results.alt_description;//Get the description
+//     const imageLink = document.createElement("a");
+//     imageLink.href = results.links.html;//Get the link for a spefic image
+//     imageLink.target = "_blank";
+//     image.textContent = result.alt_description;
 
-    imageWrapper.appendChild (image);
-    imageWrapper.appendChild (imageLink);
-    imageWrapper.appendChild (imageWrapper);
+//     imageWrapper.appendChild (image);
+//     imageWrapper.appendChild (imageLink);
+//     imageWrapper.appendChild (imageWrapper);
 
 
-});
-page ++
-if (page > 1){
-    viewmore.style.display = "block"
-}
+// });
+// page ++
+// if (page > 1){
+//     viewmore.style.display = "block"
+// }
 
 
 formEl.addEventListener("submit", (event) =>{
